357f5d237906738ed43701ba6a8f0034af3c68b3
t.parichkova <t.parichkova@ocado.com>
2022-12-08 17:43:24 +0200

    Optimize caches

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { App } from './App';
 import * as serviceWorkerRegistration from './service-worker-registration';
+
 
 import "antd/dist/antd.css";
 
