89f599f94cde4f9b082db2250bd43ec2a83e9124
t.parichkova <t.parichkova@ocado.com>
2022-12-08 14:38:40 +0200

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,16 @@
-import './realIndex';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import * as serviceWorkerRegistration from './service-worker-registration';
+
+import "antd/dist/antd.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.register();
